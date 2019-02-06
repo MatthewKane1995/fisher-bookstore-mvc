fdfe2c8d4e4c16fa1a3554d7af6ed8d61de6f0f8
Matthew Kane <kanematthew16@yahoo.com>
2019-02-06 17:46:50 -0500

    Added Views

--- Fisher.Bookstore/Controllers/BooksControllers.cs
+++ Fisher.Bookstore/Controllers/BooksControllers.cs
@@ -3,19 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Fisher.Bookstore.Controllers
 {
+    [Route("books")]
     public class BooksController : Controller
     {
+        [Route("")]
+        [Route ("Index")]
         public IActionResult Index()
         {
-            return Content("This is the Books controller's Index action");
+            return View();
         }
+        [Route("new")]
          public IActionResult New()
         {
-            return Content("This is the Books controller's New action");
+            return View();
         }
+        [Route("best-sellers")]
          public IActionResult Best()
         {
-            return Content("This is the Books controller's Best action");
+            return View();
         }
     }
 }
